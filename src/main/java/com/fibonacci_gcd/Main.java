8ad87f86767afd9bb728a52314f116821777aaec
Perry Xie <perryxie.nz@gmail.com>
2024-04-01 16:53:50 +1300

    Started a method for computing GCD. Initialises A to be the larger of two inputs.

--- src/main/java/com/fibonacci_gcd/Main.java
+++ src/main/java/com/fibonacci_gcd/Main.java
@@ -4,4 +4,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    //Method for computing the GCD of two positive integers, A and B.
+    public void GCD(int a, int b){
+        //Initialises two positive integers, A and B.
+        int A;
+        int B;
+
+        //Initialises A to be the larger of the two inputs.
+        if(a > b){
+            A = a;
+            B = b;
+        }
+        else{
+            A = b;
+            B = a;
+        }
+    }
 }
